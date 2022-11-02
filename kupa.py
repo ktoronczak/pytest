40522641f6f21202b530c20cf2cd199265ad4ad7
ktoronczak <117314990+ktoronczak@users.noreply.github.com>
2022-11-02 21:41:38 +0100

    Update kupa.py

--- kupa.py
+++ kupa.py
@@ -1,5 +1,20 @@
-import pytest
+import unittest 
 
-def suma2():
-  suma=2+2
-  assert==4
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if name == 'main':
+    unittest.main()
